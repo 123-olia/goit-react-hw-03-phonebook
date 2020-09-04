cf385a1c16c96f9efcb9a859ca74160f80cefb39
Olia_Antipenko <olia95shash@gmail.com>
2020-09-04 13:32:45 +0300

    Updates

---
+++ precache-manifest.77fb469742381e3668ce3cf8f49e0b32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b417f7f523affaf5768f44d5a852eb7a",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "c5f65b534564fdfebf5d",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.67c1a706.chunk.css"
+  },
+  {
+    "revision": "fee25b264fecfbe58554",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b814edb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b814edb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5f65b534564fdfebf5d",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.16c40c8e.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
